960ba408e6a5420011a8cf4d17ecda92d0b0313b
Mamta Bansal <mamta.bansal.mail@gmail.com>
2021-09-20 11:45:25 +0200

    Add a button

--- MyTestProject-client/src/main/java/com/example/mytestproject/client/ui/application/content/screen01/Screen01Component.java
+++ MyTestProject-client/src/main/java/com/example/mytestproject/client/ui/application/content/screen01/Screen01Component.java
@@ -4,42 +4,40 @@ import com.example.mytestproject.shared.model.MyModel;
 import com.github.nalukit.nalu.client.component.AbstractComponent;
 import elemental2.dom.HTMLElement;
 import java.lang.Override;
+import java.util.logging.Logger;
+
+import org.dominokit.domino.ui.button.Button;
 import org.dominokit.domino.ui.cards.Card;
 
-/**
- * Copyright (C) 2018 - 2019 Frank Hossfeld <frank.hossfeld@googlemail.com>
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 public class Screen01Component extends AbstractComponent<IScreen01Component.Controller, HTMLElement> implements IScreen01Component {
-  private Card card;
 
-  public Screen01Component() {
-    super();
-  }
+    private static final Logger logger = Logger.getLogger(Screen01Component.class.getName());
+    private Card card;
 
-  @Override
-  public void edit(MyModel model) {
-    // That's a good place to move your data out of the model into the widgets.
-    // 
-    // Using GWT 2.x you can use the editor framework and in this case
-    // it is a good idea to edit and flush the data inside the presenter.
-    card.setTitle(model.getActiveScreen());
-  }
+    public Screen01Component() {
+        super();
+    }
 
-  @Override
-  public void render() {
-    card = Card.create("");
-    initElement(card.element());
-  }
+    @Override
+    public void edit(MyModel model) {
+        // That's a good place to move your data out of the model into the widgets.
+        //
+        // Using GWT 2.x you can use the editor framework and in this case
+        // it is a good idea to edit and flush the data inside the presenter.
+        card.setTitle(model.getActiveScreen());
+    }
+
+    @Override
+    public void render() {
+        card = Card.create("");
+
+        Button button = Button.create("Submit");
+        card.appendChild(button);
+        button.addCss("btn-submit");
+
+        // button.setHeight("100px");
+
+        logger.info("height " + button.element().style.height.asString());
+        initElement(card.element());
+    }
 }
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # MyTestProject
+
+##Build
+
+```
+mvn clean package
+```
+
+##Run
+
+
+```
+mvn gwt:codeserver -pl *-client -am
+```
+
+```
+mvn jetty:run -pl *-server -am -Denv=dev
+```
+
